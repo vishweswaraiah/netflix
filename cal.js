c1dc4ace5697ce4e960d3ce135173fd6cd907710
vishweswaraiah <165253611+vishweswaraiah@users.noreply.github.com>
2024-09-11 14:03:43 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,8 @@
+let a = prompt("Enter frist number");
+let b = prompt("Enter Second number");
+let c = prompt("Enter operator");
+
+let random = Math.random();
+ if (random>0.1){
+    alert(a c b);
+ }
